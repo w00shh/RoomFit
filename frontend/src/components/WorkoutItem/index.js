eb9e0562b4ca8c6bb1b415e090cce2c488af7c2a
Minjae Oh <ominj98@gmail.com>
2023-06-19 21:20:09 +0900

    feat: 세트 추가 및 삭제 기능 구현 완료

--- frontend/src/components/WorkoutItem/index.js
+++ frontend/src/components/WorkoutItem/index.js
@@ -3,17 +3,37 @@ import WorkoutTitle from '../WorkoutTitle';
 import styles from './styles';
 import Icon from 'react-native-vector-icons/Feather';
 import SetItem from '../SetItem';
+import {useEffect, useState} from 'react';
+
 const WorkoutItem = props => {
+  const [set, setSet] = useState([]);
+
+  useEffect(() => {});
   const handleMotionDeletePress = id => {
     props.setWorkoutList(
       props.workoutList.filter(item => item.motion_id !== id),
     );
-    //props.setWorkoutList([{motion_id: 3, set: []}]);
   };
 
-  const handleSetDeletePress = () => {};
+  const handleSetDeletePress = id => {
+    const updatedWorkoutList = [...props.workoutList];
+    updatedWorkoutList[
+      updatedWorkoutList.findIndex(item => item.motion_id === id)
+    ].set.pop();
+    props.setWorkoutList(updatedWorkoutList);
+  };
 
-  const handleSetAddPress = () => {};
+  const handleSetAddPress = id => {
+    const updatedWorkoutList = [...props.workoutList];
+    updatedWorkoutList[
+      updatedWorkoutList.findIndex(item => item.motion_id === id)
+    ].set.push({
+      weight: 0,
+      reps: 0,
+      mode: 'normal',
+    });
+    props.setWorkoutList(updatedWorkoutList);
+  };
   return (
     <View style={styles.workoutItemContainer}>
       <WorkoutTitle motion_id={props.motion.motion_id}></WorkoutTitle>
@@ -21,11 +41,23 @@ const WorkoutItem = props => {
         isKey={true}
         isExercising={props.isExercising}
         setIsModalVisible={props.setIsModalVisible}></SetItem>
-      <SetItem
+      {props.workoutList[
+        props.workoutList.findIndex(item => item.motion_id === props.id)
+      ].set &&
+        props.workoutList[
+          props.workoutList.findIndex(item => item.motion_id === props.id)
+        ].set.map((value, key) => (
+          <SetItem
+            isKey={false}
+            isExercising={props.isExercising}
+            setIsModalVisible={props.setIsModalVisible}
+            motion={props.motion}></SetItem>
+        ))}
+      {/* <SetItem
         isKey={false}
         isExercising={props.isExercising}
         setIsModalVisible={props.setIsModalVisible}
-        motion={props.motion}></SetItem>
+        motion={props.motion}></SetItem> */}
 
       <View style={styles.buttonContainer}>
         <TouchableOpacity
@@ -40,7 +72,11 @@ const WorkoutItem = props => {
             color="#808080"></Icon>
           <Text>동작 삭제</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            handleSetDeletePress(props.id);
+          }}>
           <Icon
             style={styles.icon}
             name="minus"
@@ -48,7 +84,11 @@ const WorkoutItem = props => {
             color="#808080"></Icon>
           <Text>세트 삭제</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={styles.button}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            handleSetAddPress(props.id);
+          }}>
           <Icon
             style={styles.icon}
             name="plus"
