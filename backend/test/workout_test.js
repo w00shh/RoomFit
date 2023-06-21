19b863d00a412887c36ff9ccda8ca36b488da0d7
sun00K <ssuno0ki3@gmail.com>
2023-06-21 16:10:40 +0900

    Workout Test Update

--- backend/test/workout_test.js
+++ backend/test/workout_test.js
@@ -10,17 +10,20 @@ axios
     console.error(err);
   });
 
-//운동 종료
-axios
-  .put('http://localhost:4000/workout/done', {
-    workout_id: 1,
-    tut: '00:45:00',
-    title: '3일차 운동',
-    content: '좌우 파워 밸런스가 잘 안맞는다.',
-  })
-  .then(res => {
-    console.log(res);
-  })
-  .catch(err => {
-    console.error(err);
-  });
+setTimeout(() => {
+  //운동 종료
+  axios
+    .put('http://localhost:4000/workout/done', {
+      workout_id: 2,
+      tut: '00:45:00',
+      title: '3일차 운동',
+      content: '좌우 파워 밸런스가 잘 안맞는다.',
+    })
+    .then(res => {
+      console.log(res);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}, 7000);
+console.log('운동 종료');
