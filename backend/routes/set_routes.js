83460c4a56b788ef61f345e6c46806233921e3ca
sun00K <ssuno0ki3@gmail.com>
2023-06-21 15:45:48 +0900

    Set Router Error Resolve

--- backend/routes/set_routes.js
+++ backend/routes/set_routes.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const set_controller = require('../controllers/set_controller');
 
 router.post('/', set_controller.create_set);
-router.get('/:workout_id', set_controller.workout_set);
 
 module.exports = router;
