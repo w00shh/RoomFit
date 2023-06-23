d8537dd0431b0eb9209ecd05319ceb89bcac3fd0
W00SHH <jerry7780@g.skku.edu>
2023-06-23 14:07:12 +0900

    add email verification

--- backend/routes/account_routes.js
+++ backend/routes/account_routes.js
@@ -8,6 +8,7 @@ router.put('/login', account_controller.account_login);
 router.get('/google-auth', account_controller.google_auth);
 router.get('/google-auth/redirect', account_controller.google_auth_redirect);
 router.get('/find-id', account_controller.find_id);
-router.get('/find-password',account_controller.find_password)
+router.get('/find-password', account_controller.find_password);
+router.post('/email-verification', account_controller.email_verification);
 
 module.exports = router;
