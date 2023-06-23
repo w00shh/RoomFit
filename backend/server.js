2624c96242d01a95c75289bfd9a669fb59380fcd
sun00K <ssuno0ki3@gmail.com>
2023-06-23 09:17:45 +0900

    Server Change Route

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,9 @@ app.use(express.json());
 
 //import routers
 const workout = require('./routes/workout_routes');
-const record = require('./routes/record_routes');
-const set = require('./routes/set_routes');
 
 //router settings
 app.use('/workout', workout);
-app.use('/record', record);
-app.use('/set', set);
 
 const port = 4000;
 app.listen(port, () => {
