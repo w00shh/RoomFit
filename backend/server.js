0b9a14d6e500cfe63b0794f6c3f6626374bfdeac
sun00K <ssuno0ki3@gmail.com>
2023-06-23 13:03:44 +0900

    Backend Integration

--- backend/server.js
+++ backend/server.js
@@ -4,9 +4,13 @@ app.use(express.json());
 
 //import routers
 const workout = require('./routes/workout_routes');
+const routine = require('./routes/routine_routes');
+const motion = require('./routes/motion_routes');
 
 //router settings
 app.use('/workout', workout);
+app.use('/routine', routine);
+app.use('/motion', motion);
 
 const port = 4000;
 app.listen(port, () => {
