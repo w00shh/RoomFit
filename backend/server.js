c0b658ff6f07855d4f0552c1b5482f7bb0a898f4
sun00K <ssuno0ki3@gmail.com>
2023-06-26 10:22:00 +0900

    Add Server Router

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,16 @@ const workout = require('./routes/workout_routes');
 const record = require('./routes/record_routes');
 const account = require('./routes/account_routes');
 const set = require('./routes/set_routes');
+const routine = require('./routes/routine_routes');
+const motion = require('./routes/motion_routes');
 
 //router settings
 app.use('/workout', workout);
 app.use('/record', record);
 app.use('/set', set);
 app.use('/account', account);
+app.use('/routine', routine);
+app.use('/motion', motion);
 
 const port = 4000;
 app.listen(port, () => {
