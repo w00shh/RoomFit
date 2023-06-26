6a530da2720dbce6c78cfb1b2f26aa4a19ad89bc
sun00K <ssuno0ki3@gmail.com>
2023-06-26 09:05:56 +0900

    Add motion detail in workout detail

--- backend/models/workout_model.js
+++ backend/models/workout_model.js
@@ -61,12 +61,12 @@ Workout.get = (workout_id, callback) => {
 //Get all records & sets in workout
 Workout.detail = (workout_id, callback) => {
   db.all(
-    `SELECT record.*, (
+    `SELECT record.*, motion.motion_name, motion.imageURL, (
       SELECT json_group_array(json_object('set_no', set_info.set_no, 'weight', set_info.weight, 'rep', set_info.rep, 'mode', set_info.mode))
       FROM set_info
       WHERE set_info.record_id = record.record_id
     ) AS sets
-    FROM record
+    FROM record JOIN motion ON motion.motion_id = record.motion_id
     WHERE workout_id = ?`,
     [workout_id],
     (err, rows) => {
