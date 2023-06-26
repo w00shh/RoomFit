cf654eaa8235246bd5c167589ec379f9f03bf5b2
sun00K <ssuno0ki3@gmail.com>
2023-06-26 09:38:01 +0900

    Motion Error Change

--- backend/test/motion_test.js
+++ backend/test/motion_test.js
@@ -1,60 +1,69 @@
+const axios = require('axios');
+
 // 모션 불러오기 테스트
 
-fetch('http://127.0.0.1:4000/motion',{
-    method: "POST",
-    headers:{
-        'Content-type' : 'application/json'
-    },
-    body: JSON.stringify({
-        user_id: 1
-    })
-}).then(res=>res.json()).then(res=>{
+// axios
+//   .post('http://127.0.0.1:4000/motion', {user_id: 'user1'})
+//   .then(res => console.log(res))
+//   .catch(err => console.error(err));
+
+fetch('http://127.0.0.1:4000/motion', {
+  method: 'POST',
+  headers: {
+    'Content-type': 'application/json',
+  },
+  body: JSON.stringify({
+    user_id: 'user1',
+  }),
+})
+  .then(res => res.json())
+  .then(res => {
     console.log(res);
-});
+  });
 
-// 모션 추가하기 테스트
-fetch('http://127.0.0.1:4000/motion/add',{
-    method: "POST",
-    headers:{
-    'Content-type' : 'application/json'
-    },
-    body: JSON.stringify({
-        motion_ids: [2,3,4]
-    })
-}).then(res=>res.json()).then(res=>{
-    console.log(res);
-});
+// // 모션 추가하기 테스트
+// fetch('http://127.0.0.1:4000/motion/add',{
+//     method: "POST",
+//     headers:{
+//     'Content-type' : 'application/json'
+//     },
+//     body: JSON.stringify({
+//         motion_ids: [2,3,4]
+//     })
+// }).then(res=>res.json()).then(res=>{
+//     console.log(res);
+// });
 
-// 즐겨찾기 추가
-fetch('http://127.0.0.1:4000/motion/favInsert/1',{
-    method: "POST",
-    headers:{
-        'Content-type' : 'application/json'
-    },
-    body: JSON.stringify({
-        user_id: 1
-    })
-});
+// // 즐겨찾기 추가
+// fetch('http://127.0.0.1:4000/motion/favInsert/1',{
+//     method: "POST",
+//     headers:{
+//         'Content-type' : 'application/json'
+//     },
+//     body: JSON.stringify({
+//         user_id: 1
+//     })
+// });
 
-// 즐겨찾기 삭제
-fetch('http://127.0.0.1:4000/motion/favDelete/1',{
-    method: "POST",
-    headers:{
-        'Content-type' : 'application/json'
-    },
-    body: JSON.stringify({
-        user_id: 1
-    })
-});
+// // 즐겨찾기 삭제
+// fetch('http://127.0.0.1:4000/motion/favDelete/1',{
+//     method: "POST",
+//     headers:{
+//         'Content-type' : 'application/json'
+//     },
+//     body: JSON.stringify({
+//         user_id: 1
+//     })
+// });
 
-// 동작 검색
-fetch('http://127.0.0.1:4000/motion/search',{
-    method: "POST",
-    headers:{
-        'Content-type' : 'application/json'
-    },
-    body: JSON.stringify({
-        user_id: 1,
-        motion_name: "벤칲"
-    })
-});
+// // 동작 검색
+// fetch('http://127.0.0.1:4000/motion/search',{
+//     method: "POST",
+//     headers:{
+//         'Content-type' : 'application/json'
+//     },
+//     body: JSON.stringify({
+//         user_id: 1,
+//         motion_name: "벤칲"
+//     })
+// });
