528089a41ae09e0ecc424b953b9b97689b810b5f
Minjae Oh <ominj98@gmail.com>
2023-06-27 15:13:57 +0900

    feat: baseURL 주석처리

--- frontend/src/utils/commonAxios.js
+++ frontend/src/utils/commonAxios.js
@@ -2,5 +2,6 @@ import axios from 'axios';
 
 export const serverAxios = axios.create({
   baseURL: 'http://localhost:4000',
-  //baseURL: `${process.env.REACT_NATIVE_APP_API_ENDPOINT}`,
+  //baseURL: 'http://ec2-3-137-163-124.us-east-2.compute.amazonaws.com:4000',
+  //baseURL: `${process.env.REACT_APP_API_ENDPOINT}`,
 });
