cc9dd65a01f6cd36570d0ee6be2b087d1b946579
Minjae Oh <ominj98@gmail.com>
2023-06-27 17:29:03 +0900

    fix: baseURL 수정

--- frontend/src/utils/commonAxios.js
+++ frontend/src/utils/commonAxios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const serverAxios = axios.create({
-  baseURL: 'http://localhost:4000',
-  //baseURL: 'http://ec2-3-137-176-12.us-east-2.compute.amazonaws.com:4000',
+  //baseURL: 'http://localhost:4000',
+  baseURL: 'http://ec2-3-137-176-12.us-east-2.compute.amazonaws.com:4000',
   //baseURL: `${process.env.REACT_APP_API_ENDPOINT}`,
 });
