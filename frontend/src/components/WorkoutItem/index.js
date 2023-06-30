bfcae63cc23a46c9d7a81fdedbe4c0a6728fe0f9
Minjae Oh <ominj98@gmail.com>
2023-06-30 17:21:50 +0900

    Text 삭제

--- frontend/src/components/WorkoutItem/index.js
+++ frontend/src/components/WorkoutItem/index.js
@@ -33,7 +33,6 @@ const WorkoutItem = props => {
   };
   return (
     <View style={styles.workoutItemContainer}>
-      <Text>{String(props.motionList[props.motion_index].isMotionDoing)}</Text>
       <WorkoutTitle motion={props.motion}></WorkoutTitle>
       <SetItem
         isKey={true}
