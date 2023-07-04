dfeec0f754841391c058a3480bf50ff8ffd983a8
W00SHH <jerry7780@g.skku.edu>
2023-07-04 16:40:28 +0900

    feature: iOS 로그인 기능 추가

--- frontend/ios/frontend/AppDelegate.mm
+++ frontend/ios/frontend/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <React/RCTLinkingManager.h>
 
 @implementation AppDelegate
 
@@ -33,4 +34,12 @@
   return true;
 }
 
+- (BOOL)application:(UIApplication *)application
+openURL:(NSURL *)url
+options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+return [RCTLinkingManager application:application openURL:url options:options];
+}
+
 @end
+
