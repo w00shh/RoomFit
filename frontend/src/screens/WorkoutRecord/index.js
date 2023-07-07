d8b90057b99b53db19e0884471dafab3c9464a78
eyeonze <leeyj1007987@g.skku.edu>
2023-07-07 16:51:09 +0900

    공동작업 통계

--- frontend/src/screens/WorkoutRecord/index.js
+++ frontend/src/screens/WorkoutRecord/index.js
@@ -5,6 +5,8 @@ import {
   TouchableOpacity,
   ScrollView,
   Dimensions,
+  SafeAreaView,
+  Image,
 } from 'react-native';
 import styles from './styles';
 import {serverAxios} from '../../utils/commonAxios';
@@ -186,175 +188,193 @@ const WorkoutRecord = ({navigation, route}) => {
       ) : (
         <Image source={require('../../assets/images/line2.png')}></Image>
       )} */}
-      <View
-        style={{
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: 100,
-          backgroundColor: '#f5f5f5',
-          width: 156,
-          height: 40,
-          marginTop: 24,
-        }}>
-        <View
-          style={{
-            backgroundColor: isCalendar ? '#f5f5f5' : '#fff',
-            borderRadius: 100,
-            width: 73,
-            height: 32,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <TouchableOpacity onPress={() => setIsCalendar(false)}>
-            <Text
+      {isLeft && (
+        <View style={{alignItems: 'center'}}>
+          <View
+            style={{
+              flexDirection: 'row',
+              justifyContent: 'center',
+              alignItems: 'center',
+              borderRadius: 100,
+              backgroundColor: '#f5f5f5',
+              width: 156,
+              height: 40,
+              marginTop: 24,
+            }}>
+            <View
               style={{
-                fontSize: 14,
-                color: isCalendar ? '#808080' : '#242424',
+                backgroundColor: isCalendar ? '#f5f5f5' : '#fff',
+                borderRadius: 100,
+                width: 73,
+                height: 32,
+                justifyContent: 'center',
+                alignItems: 'center',
               }}>
-              운동기록
-            </Text>
-          </TouchableOpacity>
-        </View>
-        <View
-          style={{
-            backgroundColor: isCalendar ? '#fff' : '#f5f5f5',
-            borderRadius: 100,
-            width: 73,
-            height: 32,
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}>
-          <TouchableOpacity onPress={() => setIsCalendar(true)}>
-            <Text
+              <TouchableOpacity onPress={() => setIsCalendar(false)}>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    color: isCalendar ? '#808080' : '#242424',
+                  }}>
+                  운동기록
+                </Text>
+              </TouchableOpacity>
+            </View>
+            <View
               style={{
-                fontSize: 14,
-                color: isCalendar ? '#242424' : '#808080',
+                backgroundColor: isCalendar ? '#fff' : '#f5f5f5',
+                borderRadius: 100,
+                width: 73,
+                height: 32,
+                justifyContent: 'center',
+                alignItems: 'center',
               }}>
-              캘린더
-            </Text>
-          </TouchableOpacity>
-        </View>
-      </View>
-      <View style={{height: 16}}></View>
-      {!isCalendar && (
-        <ScrollView>
-          {workoutList.length > 0 &&
-            formattedData.map(value => (
-              <View key={value.date} style={{marginBottom: 40}}>
-                <View>
-                  <Text
-                    style={{
-                      fontSize: 16,
-                      fontWeight: '700',
-                      color: '#242424',
-                    }}>
-                    {value.date}
-                  </Text>
-                  {value.data.map((values, keys) => (
-                    <TouchableOpacity
-                      key={keys}
-                      onPress={() =>
-                        navigation.navigate('WorkoutDetail', {
-                          startingPoint: 1,
-                          selectedDate: selectedDate,
-                          workout_id: values.workout_id,
-                          title: values.title,
-                          start_time:
-                            values.start_time.split(' ')[1].split(':')[0] +
-                            ':' +
-                            values.start_time.split(' ')[1].split(':')[0],
-                          end_time:
-                            values.end_time.split(' ')[1].split(':')[0] +
-                            ':' +
-                            values.end_time.split(' ')[1].split(':')[0],
-                          targets: values.targets,
-                          total_time: values.total_time,
-                          total_weight: values.total_weight,
-                          memo: values.memo,
-                          isHomeScreen: false,
-                        })
-                      }>
-                      <RecentExercise data={values}></RecentExercise>
-                    </TouchableOpacity>
-                  ))}
-                </View>
-              </View>
-            ))}
-          <View style={{height: 50}}></View>
-        </ScrollView>
-      )}
-      {isCalendar && (
-        <ScrollView>
-          <View>
-            <Calendar
-              style={styles.Calendar}
-              monthFormat={'yyyy.M'}
-              onMonthChange={month =>
-                handleMonthChange(month.year, month.month)
-              }
-              theme={{
-                todayTextColor: '#5252fa',
-                selectedDayBackgroundColor: '#5252fa',
-                arrowColor: '#5252fa',
-              }}
-              onDayPress={day => {
-                setSelectedDate(day.dateString);
-              }}
-              markedDates={{
-                ...markedDates,
-                [selectedDate]: {
-                  selected: true,
-                  selectedColor: '#5252fa',
-                  selectedTextColor: 'white',
-                },
-              }}></Calendar>
+              <TouchableOpacity onPress={() => setIsCalendar(true)}>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    color: isCalendar ? '#242424' : '#808080',
+                  }}>
+                  캘린더
+                </Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          {selectedWorkout.length > 0 &&
-            selectedDate === selectedWorkout[0].start_time.split(' ')[0] && (
-              <>
-                <View>
-                  <Text
-                    style={{
-                      marginTop: 10,
-                      fontSize: 16,
-                      fontWeight: '700',
-                      color: '#242424',
-                    }}>
-                    {selectedDate}
-                  </Text>
-                  {selectedWorkout.map((value, key) => (
-                    <TouchableOpacity
-                      key={key}
-                      onPress={() =>
-                        navigation.navigate('WorkoutDetail', {
-                          startingPoint: 2,
-                          selectedDate: selectedDate,
-                          workout_id: value.workout_id,
-                          title: value.title,
-                          start_time:
-                            value.start_time.split(' ')[1].split(':')[0] +
-                            ':' +
-                            value.start_time.split(' ')[1].split(':')[0],
-                          end_time:
-                            value.end_time.split(' ')[1].split(':')[0] +
-                            ':' +
-                            value.end_time.split(' ')[1].split(':')[0],
-                          targets: value.targets,
-                          total_time: value.total_time,
-                          total_weight: value.total_weight,
-                          memo: value.memo,
-                          isHomeScreen: false,
-                        })
-                      }>
-                      <RecentExercise data={value}></RecentExercise>
-                    </TouchableOpacity>
-                  ))}
-                </View>
-                <View style={{height: 90}}></View>
-              </>
-            )}
-        </ScrollView>
+          <View style={{height: 16}}></View>
+          {!isCalendar && (
+            <ScrollView>
+              {workoutList.length > 0 &&
+                formattedData.map(value => (
+                  <View key={value.date} style={{marginBottom: 40}}>
+                    <View>
+                      <Text
+                        style={{
+                          fontSize: 16,
+                          fontWeight: '700',
+                          color: '#242424',
+                        }}>
+                        {value.date}
+                      </Text>
+                      {value.data.map((values, keys) => (
+                        <TouchableOpacity
+                          key={keys}
+                          onPress={() =>
+                            navigation.navigate('WorkoutDetail', {
+                              startingPoint: 1,
+                              selectedDate: selectedDate,
+                              workout_id: values.workout_id,
+                              title: values.title,
+                              start_time:
+                                values.start_time.split(' ')[1].split(':')[0] +
+                                ':' +
+                                values.start_time.split(' ')[1].split(':')[0],
+                              end_time:
+                                values.end_time.split(' ')[1].split(':')[0] +
+                                ':' +
+                                values.end_time.split(' ')[1].split(':')[0],
+                              targets: values.targets,
+                              total_time: values.total_time,
+                              total_weight: values.total_weight,
+                              memo: values.memo,
+                              isHomeScreen: false,
+                            })
+                          }>
+                          <RecentExercise data={values}></RecentExercise>
+                        </TouchableOpacity>
+                      ))}
+                    </View>
+                  </View>
+                ))}
+              <View style={{height: 50}}></View>
+            </ScrollView>
+          )}
+          {isCalendar && (
+            <ScrollView>
+              <View>
+                <Calendar
+                  style={styles.Calendar}
+                  monthFormat={'yyyy.M'}
+                  onMonthChange={month =>
+                    handleMonthChange(month.year, month.month)
+                  }
+                  theme={{
+                    todayTextColor: '#5252fa',
+                    selectedDayBackgroundColor: '#5252fa',
+                    arrowColor: '#5252fa',
+                  }}
+                  onDayPress={day => {
+                    setSelectedDate(day.dateString);
+                  }}
+                  markedDates={{
+                    ...markedDates,
+                    [selectedDate]: {
+                      selected: true,
+                      selectedColor: '#5252fa',
+                      selectedTextColor: 'white',
+                    },
+                  }}></Calendar>
+              </View>
+              {selectedWorkout.length > 0 &&
+                selectedDate ===
+                  selectedWorkout[0].start_time.split(' ')[0] && (
+                  <>
+                    <View>
+                      <Text
+                        style={{
+                          marginTop: 10,
+                          fontSize: 16,
+                          fontWeight: '700',
+                          color: '#242424',
+                        }}>
+                        {selectedDate}
+                      </Text>
+                      {selectedWorkout.map((value, key) => (
+                        <TouchableOpacity
+                          key={key}
+                          onPress={() =>
+                            navigation.navigate('WorkoutDetail', {
+                              startingPoint: 2,
+                              selectedDate: selectedDate,
+                              workout_id: value.workout_id,
+                              title: value.title,
+                              start_time:
+                                value.start_time.split(' ')[1].split(':')[0] +
+                                ':' +
+                                value.start_time.split(' ')[1].split(':')[0],
+                              end_time:
+                                value.end_time.split(' ')[1].split(':')[0] +
+                                ':' +
+                                value.end_time.split(' ')[1].split(':')[0],
+                              targets: value.targets,
+                              total_time: value.total_time,
+                              total_weight: value.total_weight,
+                              memo: value.memo,
+                              isHomeScreen: false,
+                            })
+                          }>
+                          <RecentExercise data={value}></RecentExercise>
+                        </TouchableOpacity>
+                      ))}
+                    </View>
+                    <View style={{height: 90}}></View>
+                  </>
+                )}
+            </ScrollView>
+          )}
+        </View>
+      )}
+      {!isLeft && (
+        <SafeAreaView>
+          <ScrollView>
+            <Text>ss</Text>
+            <Image
+              source={require('../../assets/images/devider.png')}
+              style={{
+                width: 390 * width_ratio,
+                height: 8 * height_ratio,
+              }}></Image>
+          </ScrollView>
+        </SafeAreaView>
       )}
 
       <View style={styles.navigator}>
