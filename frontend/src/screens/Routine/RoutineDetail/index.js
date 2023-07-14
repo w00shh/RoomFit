7313881d6ed76f780423e4bc56eb227a166cac5a
sun00K <ssuno0ki3@gmail.com>
2023-07-14 13:50:52 +0900

    Routine Modify SVG

--- frontend/src/screens/Routine/RoutineDetail/index.js
+++ frontend/src/screens/Routine/RoutineDetail/index.js
@@ -10,8 +10,6 @@ import {
   Dimensions,
 } from 'react-native';
 import styles from './styles';
-import Edit from 'react-native-vector-icons/Entypo';
-import Back from 'react-native-vector-icons/Ionicons';
 import WorkoutItem from '../../../components/WorkoutItem';
 import {serverAxios} from '../../../utils/commonAxios';
 import CustomButton_W from '../../../components/CustomButton_W';
@@ -22,6 +20,12 @@ import {
   GestureHandlerRootView,
   gestureHandlerRootHOC,
 } from 'react-native-gesture-handler';
+
+//svg
+import Modify from '../../../assets/svg/buttons/single/modify.svg';
+import Back from '../../../assets/svg/buttons/single/back.svg';
+
+import {Divider} from '../../../components/divider';
 
 const width_ratio = Dimensions.get('screen').width / 390;
 const height_ratio = Dimensions.get('screen').height / 844;
@@ -124,15 +128,17 @@ const RoutineDetail = ({navigation, route}) => {
             handleBackButton();
             //navigation.reset({routes: [{name: 'MyRoutine'}]});
           }}>
-          <Back
-            name="arrow-back"
-            color={'#242424'}
-            size={25 * height_ratio}></Back>
+          <Back height={24 * height_ratio} width={24 * width_ratio} />
         </TouchableOpacity>
       ),
 
       headerTitle: () => (
-        <>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: 4 * width_ratio,
+          }}>
           <Text
             style={{
               marginHorizontal: 6 * width_ratio,
@@ -146,9 +152,9 @@ const RoutineDetail = ({navigation, route}) => {
             onPress={() => {
               setIsRoutineNameModalVisible(!isRoutineNameModalVisible);
             }}>
-            <Edit name="edit" size={16 * height_ratio} color="#808080"></Edit>
+            <Modify height={16 * height_ratio} width={16 * width_ratio} />
           </TouchableOpacity>
-        </>
+        </View>
       ),
       headerRight: () => (
         <TouchableOpacity
@@ -310,18 +316,24 @@ const RoutineDetail = ({navigation, route}) => {
 
   const renderItem = gestureHandlerRootHOC(({item, index, drag, isActive}) => {
     return (
-      <WorkoutItem
-        drag={drag}
-        isActive={isActive}
-        motion_index={item.motion_index}
-        id={item.motion_id}
-        motion={item}
-        isExercising={false}
-        setIsModalVisible={setIsModalVisible}
-        motion={item}
-        motionList={motionList}
-        setMotionList={setMotionList}
-        setSelectedMode={setSelectedMode}></WorkoutItem>
+      <>
+        <WorkoutItem
+          drag={drag}
+          isActive={isActive}
+          motion_index={item.motion_index}
+          id={item.motion_id}
+          motion={item}
+          isExercising={false}
+          setIsModalVisible={setIsModalVisible}
+          motion={item}
+          motionList={motionList}
+          setMotionList={setMotionList}
+          setSelectedMode={setSelectedMode}
+        />
+        {!isActive && item !== motionList[motionList.length - 1] && (
+          <Divider height_ratio={height_ratio} />
+        )}
+      </>
     );
   });
 
