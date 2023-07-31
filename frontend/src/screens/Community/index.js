5e42ffeaec4147831dd8faa0b3b1a970d46029ce
Minjae Oh <ominj98@gmail.com>
2023-07-31 17:04:36 +0900

    feat: 커뮤니티 style 수정 중

--- frontend/src/screens/Community/index.js
+++ frontend/src/screens/Community/index.js
@@ -170,7 +170,7 @@ const Comment = ({
             onPress={() => {
               setIsSettingModal(true);
             }}>
-            <Icons_two name="dots-three-vertical" size={18 * height_ratio} />
+            <Icons_two name="dots-three-vertical" size={12 * height_ratio} />
           </TouchableOpacity>
         </View>
       </View>
