2d57ffe1903ee09b8a7914716c40a5872060b15c
eyeonze <leeyj1007987@g.skku.edu>
2023-07-31 17:20:33 +0900

    community

--- frontend/src/screens/Community/styles.js
+++ frontend/src/screens/Community/styles.js
@@ -46,6 +46,7 @@ const styles = StyleSheet.create({
   FeedBottom: {
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'space-between',
   },
   Likes: {
     flexDirection: 'row',
